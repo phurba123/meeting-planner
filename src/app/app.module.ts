f0f869faa3c023a771346a18539c586cc862fd5e
phursang <psherpa592@gmail.com>
2020-02-18 15:22:29 +0530

    included FomrsModule in userModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserModule } from './user/user.module';
 
+import {FormsModule} from '@angular/forms'
+
 
 @NgModule({
   declarations: [
@@ -13,7 +15,8 @@ import { UserModule } from './user/user.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    UserModule
+    UserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
