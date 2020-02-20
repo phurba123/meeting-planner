8e79edce28eaf54fadaf151bef21125bf2a6dc2b
phursang <psherpa592@gmail.com>
2020-02-20 23:35:50 +0530

    signup completed(previous was empty commit)

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -10,20 +10,19 @@ export class UserService {
   private backendUrl = 'http://localHost:3000/api/v1/user';
 
   constructor(private http: HttpClient) { }
-    //end of constructor
+  //end of constructor
 
-    //for signup
-    public signUp(data):Observable<any>
-    {
-      const params = new HttpParams()
-      .set('firstName',data.firstName)
-      .set('lastName',data.lastName)
-      .set('userName',data.userName)
-      .set('mobileNumber',data.mobileNumber)
-      .set('email',data.email)
-      .set('password',data.password);
+  //for signup
+  public signUp(data): Observable<any> {
+    const params = new HttpParams()
+      .set('firstName', data.firstName)
+      .set('lastName', data.lastName)
+      .set('userName', data.userName)
+      .set('mobileNumber', data.mobileNumber)
+      .set('email', data.email)
+      .set('password', data.password);
 
-      return this.http.post(`${this.backendUrl}/signup`,params);
-    }
-    //end of signup
+    return this.http.post(`${this.backendUrl}/signup`, params);
+  }
+  //end of signup
 }
