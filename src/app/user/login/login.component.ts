c9c2915291483a1a1f24b34f9e02777c81b7bc6e
phursang <psherpa592@gmail.com>
2020-02-21 00:01:43 +0530

    logIn Successfull

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {ToastrService} from 'ngx-toastr';
+import { ToastrService } from 'ngx-toastr';
+import { UserService } from '../../user.service';
+import {Router} from '@angular/router'
 
 @Component({
   selector: 'app-login',
@@ -8,35 +10,56 @@ import {ToastrService} from 'ngx-toastr';
 })
 export class LoginComponent implements OnInit {
 
-  public email:any;
-  public password:any;
+  public email: any;
+  public password: any;
 
   constructor(
-    private toast:ToastrService
-  ) 
-  { }
+    private toast: ToastrService,
+    private userService: UserService,
+    private router:Router
+  ) { }
 
   ngOnInit() {
   }
 
-  public logIn():void{
-    if(!this.email){
-      this.toast.warning('email cannot be empty','enter email');
+  public logIn(): void {
+    if (!this.email) {
+      this.toast.warning('email cannot be empty', 'enter email');
     }
-    else if(!this.password)
-    {
-      this.toast.warning('password cannot be empty','enter password')
+    else if (!this.password) {
+      this.toast.warning('password cannot be empty', 'enter password')
     }
-    else{
-      
+    else {
+      let user =
+      {
+        email: this.email,
+        password: this.password
+      }
+
+      this.userService.logIn(user).subscribe(
+        (data) => {
+          if(data.status === 200)
+          {
+            this.toast.success('You Are Logged In','Login Successfull');
+
+            setTimeout(()=>
+            {
+              this.router.navigate(['role/normal']);
+            })
+          }
+          else{
+            this.toast.error('Some Error Occured','Login Error');
+          }
+        }
+        , (err) => {
+
+        })
     }
   }
 
   //login using enterkey event
-  public loginUsingKeyPress(event:any)
-  {
-    if(event.keyCode==13)
-    {
+  public loginUsingKeyPress(event: any) {
+    if (event.keyCode == 13) {
       this.logIn();
     }
   }
